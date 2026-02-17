00596fdc94d76256459c82ff22f19fcc80b0386f
sileshkatakamsetti <sileshkatakamsetti@gmail.com>
2026-02-17 15:18:14 +0530

    fix client chat and dashboards

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://freelance-backend-uvp9.onrender.com",
+  baseURL: "http://localhost:5000",
 });
 
 API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
+  const user = JSON.parse(localStorage.getItem("user"));
+  if (user?.token) {
+    req.headers.Authorization = `Bearer ${user.token}`;
   }
   return req;
 });
